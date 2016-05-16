65c3bc43282c028219c9c269e116061952b74e6d
rohan mahajan <rohanm@31-33-30.wireless.csail.mit.edu>
2016-05-15 21:16:19 -0400

    more cleanup

--- graphs/broadcastjoinrdd.py
+++ graphs/broadcastjoinrdd.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-plt.plot([1,2,3,4], [1,4,9,16], 'ro')
-plt.plot([1,2,3,4], [2,5,8,13], 'bo')
+plt.plot([1,2.5,5,7.5, 10], [1.3,1.5,1.56,1.6, 1.7], 'r')
+plt.plot([1,2.5,5,7.5, 10], [0.55,0.75,1,1.2, 1.4], 'b')
 plt.ylabel('minutes')
 plt.xlabel('rdd size')
 plt.show()
