a54cd7bd373917145affc0df4baf599a91b4739e
rohan mahajan <rohanm@31-33-30.wireless.csail.mit.edu>
2016-05-16 01:47:04 -0400

    adding more results

--- graphs/broadcastjoinrdd.py
+++ graphs/broadcastjoinrdd.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-plt.plot([1,2.5,5,7.5, 10], [1.3,1.5,1.56,1.6, 1.7], 'r')
-plt.plot([1,2.5,5,7.5, 10], [0.55,0.75,1,1.2, 1.4], 'b')
-plt.ylabel('minutes')
-plt.xlabel('rdd size')
+plt.plot([1,2.5,5,7.5, 10, 10.06], [1.3,1.5,1.56,1.6, 1.7, 1.76], 'ro')
+plt.plot([1,2.5,5,7.5, 10, 10.06], [0.55,0.75,1,1.2, 1.5, 2.15], 'bo')
+plt.ylabel('Minutes')
+plt.xlabel('Millions of Key-Pairs')
 plt.show()
